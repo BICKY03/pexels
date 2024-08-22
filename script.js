583ee3b48b4d852e7b3f790c825a4657b72abda6
Jotiraj Majhi <127618478+BICKY03@users.noreply.github.com>
2024-08-22 10:29:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const similarProductsCarousel = new Splide('#similar-products-carousel', {
 }).mount();
 
 const favouritesCarousel = new Splide('#favourites-carousel', {
-    type       : 'loop',
+    // type       : 'loop',
     perPage    : 3,
     gap        : '10px',
     pagination : false,
